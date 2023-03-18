11ffdf4a03b4ed94f481418dba9fe0d97dc3a131
francorbacho <francorbacho@proton.me>
2023-03-18 19:51:12 +0100

    cheng/move: Include promoted piece in PseudoMove as Display

--- cheng/src/movement.rs
+++ cheng/src/movement.rs
@@ -153,6 +153,15 @@ impl FromStr for PseudoMove {
 
 impl Display for PseudoMove {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}{:?}", self.origin, self.destination)
+        let Self {
+            origin,
+            destination,
+            ..
+        } = self;
+
+        match self.kind {
+            MoveKind::Move | MoveKind::Castle(_) => write!(f, "{origin:?}{destination:?}"),
+            MoveKind::Promote(piece) => write!(f, "{origin:?}{destination:?}{}", char::from(piece)),
+        }
     }
 }

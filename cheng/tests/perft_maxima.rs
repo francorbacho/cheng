2b2c58c4e52213c2bccaa77e012923e34b09783e
francorbacho <francorbacho@proton.me>
2023-04-06 18:30:08 +0200

    cheng/perft: Add perft for the position with the most moves

---
+++ cheng/tests/perft_maxima.rs
@@ -0,0 +1,13 @@
+mod perft;
+use perft::perft;
+
+use cheng::Board;
+
+#[test]
+fn perft_maxima() {
+    cheng::init();
+
+    // https://www.chessprogramming.org/Chess#Chess_Maxima
+    let board = Board::from_fen("R6R/3Q4/1Q4Q1/4Q3/2Q4Q/Q4Q2/pp1Q4/kBNN1KB1 w - - 0 1").unwrap();
+    assert_eq!(perft(&board, 1), 218)
+}

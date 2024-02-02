da92f0259f7960eda2c7ba4b91c1d820e1290169
francorbacho <francorbacho@proton.me>
2024-02-02 18:36:37 +0100

    cheng-cmd: Workaround to get `go` working for now
    Since we don't evaluate indefinitely, we don't care about the `stop`.

--- cheng-cmd/src/uci.rs
+++ cheng-cmd/src/uci.rs
@@ -47,6 +47,8 @@ pub fn position(context: &mut Context, parts: &[&str]) -> Result<(), String> {
 
 pub fn go(context: &mut Context, parts: &[&str]) -> Result<(), String> {
     let movetime = match parts[1..] {
+        // FIXME: Workaround to get `go` working.
+        [] => "0",
         ["movetime", movetime] => movetime,
         ["wtime", wtime, "btime", _btime] => wtime,
         ["wtime", wtime, "btime", _btime, "winc", _winc, "binc", _binc] => wtime,

4fb5516d628128ef33f512cb4dc7320c76d99bf3
francorbacho <francorbacho@proton.me>
2025-01-10 22:20:32 +0100

    franfish: Increase value of king

--- franfish/src/lib.rs
+++ franfish/src/lib.rs
@@ -111,7 +111,7 @@ fn evaluate<I: Inspector>(board: &BorkedBoard) -> Evaluation {
             Piece::Bishop => 325,
             Piece::Rook => 500,
             Piece::Queen => 900,
-            Piece::King => 0,
+            Piece::King => 1 << 16,
         }
     }
 
